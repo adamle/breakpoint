dc0d15de04d7cdf2f9ef67b95571cae439d3120b
AdamLe <duongle92vn@gmail.com>
2017-12-10 23:49:34 +0200

    AuthVC and LoginVC UI

---
+++ breakpoint/View/InsetTextField.swift
@@ -0,0 +1,34 @@
+//
+//  InsetTextField.swift
+//  breakpoint
+//
+//  Created by Le Dang Dai Duong on 12/10/17.
+//  Copyright © 2017 Le Dang Dai Duong. All rights reserved.
+//
+
+import UIKit
+
+class InsetTextField: UITextField {
+    
+    private var padding = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let placeholder = NSAttributedString(string: self.placeholder!, attributes: [NSAttributedStringKey.foregroundColor: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)])
+        self.attributedPlaceholder = placeholder
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        // Take a rect, inset it to return another rect
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+
+}
---
+++ breakpoint/View/ShadowView.swift
@@ -0,0 +1,20 @@
+//
+//  ShadowView.swift
+//  breakpoint
+//
+//  Created by Le Dang Dai Duong on 12/10/17.
+//  Copyright © 2017 Le Dang Dai Duong. All rights reserved.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+    
+    override func awakeFromNib() {
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowRadius = 5
+        self.layer.shadowColor = UIColor.black.cgColor
+        super.awakeFromNib()
+    }
+    
+}
