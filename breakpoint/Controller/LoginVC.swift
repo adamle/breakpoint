8cb16940ef4430868ea1f4ec1f9e0a064320a39b
AdamLe <duongle92vn@gmail.com>
2017-12-12 17:12:50 +0200

    AuthService setup

--- breakpoint/Controller/LoginVC.swift
+++ breakpoint/Controller/LoginVC.swift
@@ -10,26 +10,57 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var emailTxtField: InsetTextField!
+    @IBOutlet weak var passwordTxtField: InsetTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        emailTxtField.delegate = self
+        passwordTxtField.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func signInBtnPressed(_ sender: Any) {
+        if emailTxtField.text != nil && passwordTxtField.text != nil {
+            AuthService.instance.loginUser(withEmail: emailTxtField.text!, andPassword: passwordTxtField.text!, loginComplete: { (success, loginError) in
+                if success {
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    print(String(describing: loginError?.localizedDescription))
+                }
+                AuthService.instance.registerUser(withEmail: self.emailTxtField.text!, andPassword: self.passwordTxtField.text!, userCreationComplete: { (success, registrationError) in
+                    if success {
+                        AuthService.instance.loginUser(withEmail: self.emailTxtField.text!, andPassword: self.passwordTxtField.text!, loginComplete: { (success, nil) in
+                            print("Successfully register user")
+                            self.dismiss(animated: true, completion: nil)
+                        })
+                    } else {
+                        print(String(describing: registrationError?.localizedDescription))
+                    }
+                })
+            })
+        }
     }
-    */
-
+    
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
+
+extension LoginVC: UITextFieldDelegate { }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ breakpoint/Services/AuthService.swift
@@ -0,0 +1,56 @@
+//
+//  AuthService.swift
+//  breakpoint
+//
+//  Created by Le Dang Dai Duong on 12/12/17.
+//  Copyright © 2017 Le Dang Dai Duong. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    static let instance = AuthService()
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping(_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            
+            // providerID can be Facebook, Google, Firebase, or email
+            let userData = [
+                "provider": user.providerID,
+                "email": user.email
+            ]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+    }
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping(_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            if error != nil {
+                loginComplete(false, error)
+                return
+            }
+            loginComplete(true, nil)
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
