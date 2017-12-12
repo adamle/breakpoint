5f866f72b3df28baa830dce618b8d5b89ec7e2e9
AdamLe <duongle92vn@gmail.com>
2017-12-12 20:37:02 +0200

    UI MeVC

---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,25 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by Le Dang Dai Duong on 12/12/17.
+//  Copyright © 2017 Le Dang Dai Duong. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func signOutBtnPressed(_ sender: Any) {
+    }
+    
+}
